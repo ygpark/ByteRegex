1f6daae4f4e0f438661934dea79b6af49f367fe4
영기 박 <ghostyak@gmail.com>
2021-10-19 01:03:04 +0900

    폴더이름 수정

--- ByteRegex0/ByteRegex.cs
+++ ByteRegex0/ByteRegex.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LightweightBinRegex
+namespace ByteRegex0
 {
     internal enum CommandCode
     {
--- Test/Program.cs
+++ Test/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using LightweightBinRegex;
+using ByteRegex0;
 
 namespace Test
 {
